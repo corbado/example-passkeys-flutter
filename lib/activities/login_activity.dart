502f8bcce6033b3dd2eae56df4198c0c1eb823aa
Nicolai Stein <nicolai_stein@outlook.de>
2022-08-26 13:44:36 +0200

    design, image

--- lib/activities/login_activity.dart
+++ lib/activities/login_activity.dart
@@ -1,6 +1,6 @@
+import 'package:corbado_demo/theme/theme.dart';
 import 'package:corbado_demo/webauthn/corbado_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginActivity extends StatefulWidget {
   @override
@@ -9,41 +9,42 @@ class LoginActivity extends StatefulWidget {
 
 class _LoginActivityState extends State<LoginActivity> {
   TextEditingController usernameController = TextEditingController();
-  static const platform = MethodChannel("com.corbado.flutterapp/webauthn");
-
-  void auth() async {
-    var registerRes =
-        await CorbadoService().registerInit(usernameController.text);
-    try {
-      debugPrint("before login auth");
-      final int result =
-          await platform.invokeMethod("webauthnRegister", registerRes);
-      debugPrint("login auth result: $result");
-    } on PlatformException catch (e) {
-      debugPrint("Platform Exception meeeeep $e");
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Login")),
-        body: SingleChildScrollView(
-            padding: const EdgeInsets.all(8),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Expanded(
-                      child: TextField(
-                    controller: usernameController,
-                    decoration: InputDecoration(hintText: "Email"),
-                  )),
-                  ElevatedButton(
+        backgroundColor: corbadoDark,
+        body: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Image.asset(
+                  "assets/corbado_logo.png",
+                  scale: 2,
+                  color: Colors.white70,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 150),
+                    child: TextField(
+                      controller: usernameController,
+                      decoration: const InputDecoration(hintText: "Email"),
+                    )),
+                Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: ElevatedButton(
                       onPressed: () {
                         debugPrint("elevated button clicked");
-                        CorbadoService().register(context, "Test@web.de");
+                        CorbadoService()
+                            .register(context, usernameController.text);
                       },
-                      child: Text("Register"))
-                ])));
+                      child: const Text(
+                        "Registrieren",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ))
+              ],
+            )));
   }
 }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,15 +4,30 @@ import 'package:flutter/material.dart';
 Color corbadoDark = const Color(0xff090f1f);
 
 ThemeData theme = ThemeData(
-// This is the theme of your application.
-//
-// Try running your application with "flutter run". You'll see the
-// application has a blue toolbar. Then, without quitting the app, try
-// changing the primarySwatch below to Colors.green and then invoke
-// "hot reload" (press "r" in the console where you ran "flutter run",
-// or simply save your changes to "hot reload" in a Flutter IDE).
-// Notice that the counter didn't reset back to zero; the application
-// is not restarted.
   primaryColor: corbadoDark,
-  primarySwatch: CustomMaterialColor(corbadoDark),
+  primarySwatch: CustomMaterialColor(Colors.white70),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+    primary: CustomMaterialColor(Colors.white70),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30), // <-- Radius
+    ),
+    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+    //             primary: Color(0xFF0E72B5), // This is what you need!
+  )),
+  inputDecorationTheme: InputDecorationTheme(
+    suffixIconColor: Colors.grey.shade400,
+    hintStyle: TextStyle(color: Colors.grey.shade400),
+    fillColor: Colors.grey.shade100,
+    filled: true,
+    contentPadding: EdgeInsets.symmetric(horizontal: 18),
+    enabledBorder: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30)),
+      borderSide: BorderSide(color: Colors.transparent, width: 1.0),
+    ),
+    focusedBorder: const OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(30)),
+      borderSide: BorderSide(color: Colors.transparent, width: 1.0),
+    ),
+  ),
 );
