1dc911562f42c712ac0fa83b3bea831115188667
Nicolai Stein <nicolai_stein@outlook.de>
2023-04-06 13:56:57 +0200

    reworked corbadoservice

--- lib/api/corbado_service.dart
+++ lib/api/corbado_service.dart
@@ -6,39 +6,36 @@ import 'package:http/http.dart' as http;
 
 ///Issues api calls to the corbado backend
 class CorbadoService {
-  String apiSecret;
-  String projectID;
-  dynamic header;
+  final String apiSecret;
+  final String projectID;
 
-  var baseUrlAPI = "https://api.corbado.com/v1";
-  var baseUrlAuth = "https://auth.corbado.com/v1";
+  static const baseUrlAPI = "https://api.corbado.com/v1";
+  static const baseUrlAuth = "https://auth.corbado.com/v1";
 
-  var clientInfo = {};
-  bool canAuthenticateWithBiometrics = false;
-  bool isDeviceSupported = false;
+  const CorbadoService(this.apiSecret, this.projectID);
 
-  CorbadoService(this.apiSecret, this.projectID) {
-    header = <String, String>{
+  Map<String, String> getHeader() {
+    return <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
       'authorization':
           "Basic ${base64.encode(utf8.encode('$projectID:$apiSecret'))}"
     };
   }
 
-  Future<void> _init() async {
+  Object getClientInfo() async {
     String ipv4 = await Ipify.ipv4();
-    clientInfo = {
+    var clientInfo = {
       "userAgent": "Corbado Demo Flutter App",
       "remoteAddress": ipv4
     };
     debugPrint("_init clientInfo: $clientInfo");
+    return clientInfo;
   }
 
   Future<String> signInInit(BuildContext context, String email) async {
-    await _init();
     var value = await http.post(
         Uri.parse("$baseUrlAuth/users/passkey/login/start"),
-        headers: header,
+        headers: await getHeader(),
         body: jsonEncode({"username": email}));
 
     debugPrint("signInInit body: ${value.body}");
@@ -57,7 +54,6 @@ class CorbadoService {
       String authenticatorData,
       String signature,
       String userHandle) async {
-    await _init();
     debugPrint("flutter signInFinish called");
     debugPrint("clientDataJSON: " + clientDataJSON);
     var pubCred = {
@@ -74,7 +70,7 @@ class CorbadoService {
 
     var value = await http.post(
         Uri.parse("$baseUrlAuth/users/passkey/login/finish"),
-        headers: header,
+        headers: getHeader(),
         body: jsonEncode({"signedChallenge": jsonEncode(pubCred)}));
 
     debugPrint("signInFinish body: ${value.body}");
@@ -89,10 +85,9 @@ class CorbadoService {
   }
 
   Future<String> registerInit(BuildContext context, String email) async {
-    await _init();
     var value =
         await http.post(Uri.parse("$baseUrlAuth/users/passkey/register/start"),
-            headers: header,
+            headers: getHeader(),
             body: jsonEncode({
               "username": email,
               "fullName": email,
@@ -109,7 +104,6 @@ class CorbadoService {
 
   Future<bool> registerFinish(BuildContext context, String id, String rawId,
       String clientDataJSON, String attestationObject) async {
-    await _init();
     debugPrint("clientDataJSON: " + clientDataJSON);
     var pubCred = {
       "type": "public-key",
@@ -126,33 +120,12 @@ class CorbadoService {
 
     var value = await http.post(
         Uri.parse("$baseUrlAuth/users/passkey/register/finish"),
-        headers: header,
+        headers: getHeader(),
         body: jsonEncode({"signedChallenge": jsonEncode(pubCred)}));
 
     debugPrint("registerFinish body: ${value.body}");
     if (value.statusCode == 500) {
       throw Exception(value.body);
-    }
-    return value.statusCode == 200;
-  }
-
-  Future<bool> addOrigin(
-      BuildContext context, String name, String origin, String rpID) async {
-    await _init();
-    debugPrint("addOrigin called: $name, $origin, $rpID");
-
-    var value = await http.post(Uri.parse("$baseUrlAPI/webauthn/settings"),
-        headers: header,
-        body: jsonEncode({
-          "name": name,
-          "origin": origin,
-          "rpID": rpID,
-          "clientInfo": clientInfo
-        }));
-
-    debugPrint("addOrigin body: ${value.body}");
-    if (value.statusCode != 200 && !value.body.contains("already exists")) {
-      throw Exception("Could not add origin \n${value.body}");
     }
     return value.statusCode == 200;
   }
