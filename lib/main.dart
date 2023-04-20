36f6a931be09019485858fad14af717b13c5564d
Nicolai Stein <nicolai_stein@outlook.de>
2023-04-20 17:39:44 +0200

    Enable deeplinks

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:corbado_demo/activities/login_activity.dart';
 import 'package:corbado_demo/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:oktoast/oktoast.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(MaterialApp.router(routerConfig: router));
 }
+
+/// This handles '/' and '/details'.
+final router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (_, __) => MyApp()),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
