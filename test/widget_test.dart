01bf44955709bbc2a3571cd2d32db0486df4b470
Nicolai Stein <nicolai_stein@outlook.de>
2023-07-26 08:29:09 +0200

    add corbado_auth package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,17 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:corbado_demo/activities/login_activity.dart';
+import 'package:corbado_demo/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:corbado_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(
+      homeWidget: LoginActivity(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
