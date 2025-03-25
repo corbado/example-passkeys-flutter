9d9ae2cb4a1f6d70cbc6d184f055b4cb03ed6843
Lukas Kratzel <lukas.kratzel2004@gmail.com>
2025-03-25 08:43:15 +0100

    Automated

--- lib/pages/passkey_list_page.dart
+++ lib/pages/passkey_list_page.dart
@@ -1,7 +1,6 @@
 import 'package:corbado_auth/corbado_auth.dart';
 import 'package:corbado_auth_example/auth_provider.dart';
 import 'package:corbado_auth_example/screens/helper.dart';
-import 'package:corbado_auth_example/widgets/debug_info.dart';
 import 'package:corbado_auth_example/widgets/filled_text_button.dart';
 import 'package:corbado_auth_example/widgets/outlined_text_button.dart';
 import 'package:corbado_auth_example/widgets/passkey_card.dart';
@@ -35,122 +34,115 @@ class PasskeyListPage extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Corbado authentication')),
-      body: Stack(
-        children: [
-          DebugInfo(),
-          Center(
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Check your passkeys',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    Column(
-                      children: passkeys
-                          .map(
-                            (p) => SizedBox(
-                              width: double.infinity,
-                              child: PasskeyCard(
-                                passkey: p,
-                                onDelete: (String credentialID) async {
-                                  if (isLoading.value) {
-                                    return;
-                                  }
-                                  isLoading.value = true;
-                                  error.value = null;
-
-                                  try {
-                                    await corbado.deletePasskey(
-                                      credentialID: credentialID,
-                                    );
-
-                                    showSimpleNotification(
-                                        const Text(
-                                          'Passkey has been deleted successfully.',
-                                          style: TextStyle(color: Colors.white),
-                                        ),
-                                        leading: const Icon(
-                                          Icons.check,
-                                          color: Colors.green,
-                                        ),
-                                        background: Theme.of(context)
-                                            .colorScheme
-                                            .primary);
-                                  } on CorbadoError catch (e) {
-                                    error.value = e.translatedError;
-                                  } catch (e) {
-                                    error.value = e.toString();
-                                  } finally {
-                                    isLoading.value = false;
-                                  }
-                                },
-                              ),
-                            ),
-                          )
-                          .toList(),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FilledTextButton(
-                        onTap: () async {
-                          if (isLoading.value) {
-                            return;
-                          }
-
-                          isLoading.value = true;
-                          error.value = null;
-
-                          try {
-                            await corbado.appendPasskey();
-                            showSimpleNotification(
-                                const Text(
-                                  'Passkey has been created successfully.',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                leading: const Icon(
-                                  Icons.check,
-                                  color: Colors.green,
-                                ),
-                                background:
-                                    Theme.of(context).colorScheme.primary);
-                          } on CorbadoError catch (e) {
-                            error.value = e.translatedError;
-                          } catch (e) {
-                            error.value = e.toString();
-                          } finally {
-                            isLoading.value = false;
-                          }
-                        },
-                        content: 'Add passkey',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: OutlinedTextButton(
-                        onTap: context.pop,
-                        content: 'Back',
-                      ),
-                    ),
-                  ],
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Check your passkeys',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
+                Column(
+                  children: passkeys
+                      .map(
+                        (p) => SizedBox(
+                          width: double.infinity,
+                          child: PasskeyCard(
+                            passkey: p,
+                            onDelete: (String credentialID) async {
+                              if (isLoading.value) {
+                                return;
+                              }
+                              isLoading.value = true;
+                              error.value = null;
+
+                              try {
+                                await corbado.deletePasskey(
+                                  credentialID: credentialID,
+                                );
+
+                                showSimpleNotification(
+                                    const Text(
+                                      'Passkey has been deleted successfully.',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    leading: const Icon(
+                                      Icons.check,
+                                      color: Colors.green,
+                                    ),
+                                    background:
+                                        Theme.of(context).colorScheme.primary);
+                              } on CorbadoError catch (e) {
+                                error.value = e.translatedError;
+                              } catch (e) {
+                                error.value = e.toString();
+                              } finally {
+                                isLoading.value = false;
+                              }
+                            },
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: FilledTextButton(
+                    onTap: () async {
+                      if (isLoading.value) {
+                        return;
+                      }
+
+                      isLoading.value = true;
+                      error.value = null;
+
+                      try {
+                        await corbado.appendPasskey();
+                        showSimpleNotification(
+                            const Text(
+                              'Passkey has been created successfully.',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            leading: const Icon(
+                              Icons.check,
+                              color: Colors.green,
+                            ),
+                            background: Theme.of(context).colorScheme.primary);
+                      } on CorbadoError catch (e) {
+                        error.value = e.translatedError;
+                      } catch (e) {
+                        error.value = e.toString();
+                      } finally {
+                        isLoading.value = false;
+                      }
+                    },
+                    content: 'Add passkey',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: OutlinedTextButton(
+                    onTap: context.pop,
+                    content: 'Back',
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,7 +5,6 @@ import 'package:corbado_auth_example/widgets/outlined_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import '../widgets/debug_info.dart';
 
 class ProfilePage extends ConsumerWidget {
   ProfilePage({super.key});
@@ -17,74 +16,69 @@ class ProfilePage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Corbado authentication')),
-      body: Stack(
-        children: [
-          DebugInfo(),
-          Center(
-            child: Container(
-              constraints: const BoxConstraints(maxWidth: 500),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Welcome',
-                      style: TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      user.value?.email ?? '',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    const Text(
-                      'You are currently logged in. You have a JWT token that you can use to make calls to your backend.',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FilledTextButton(
-                        onTap: () => context.push(Routes.editProfile),
-                        content: 'Edit profile',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: FilledTextButton(
-                        onTap: () => context.push(Routes.passkeyList),
-                        content: 'Passkey list',
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child:
-                      OutlinedTextButton(
-                        onTap: corbado.signOut,
-                        content: 'Sign out',
-                      ),
-                    ),
-                  ],
+      body: Center(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 500),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Welcome',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 10),
+                Text(
+                  user.value?.email ?? '',
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'You are currently logged in. You have a JWT token that you can use to make calls to your backend.',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: FilledTextButton(
+                    onTap: () => context.push(Routes.editProfile),
+                    content: 'Edit profile',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: FilledTextButton(
+                    onTap: () => context.push(Routes.passkeyList),
+                    content: 'Passkey list',
+                  ),
+                ),
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child:
+                  OutlinedTextButton(
+                    onTap: corbado.signOut,
+                    content: 'Sign out',
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
